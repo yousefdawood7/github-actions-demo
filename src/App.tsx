2bd849fd78329a3685808e0fb048580875a2da64
Yousef Dawood <78578298+yousefdawood7@users.noreply.github.com>
2026-08-23 09:58:32 +0000

    change something

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.cssa'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
+      <section id="centder">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
